4e3073465e404fc7a33972a2078217512c65c435
Cessalan <31516973+Cessalan@users.noreply.github.com>
2019-12-12 10:05:41 -0500

    Initial commit

---
+++ inc/nav-bar.php
@@ -0,0 +1,22 @@
+
+<!-- Nav -->
+<nav id="nav">
+    <ul>
+        <li class="current"><a href="/">Home</a></li>
+        <li>
+            <a href="#" class="dropdown">Services</a>
+            <ul>
+                <li><a href="#">Horaire d'ouverture</a></li>
+                <li><a href="#">Membres</a></li>
+                <li>
+                    <a href="#">Rendez-vous</a>
+                    <ul>
+                        <li><a href="#">Prendre rendez-vous</a></li>
+                        <li><a href="#">Annuler</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
---
+++ models/Account.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fatsy
+ * Date: 2019-12-12
+ * Time: 9:46 AM
+ */
+
+class Account
+{
+
+}
