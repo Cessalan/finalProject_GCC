7e89509404d82aa02d2774c2363351af8d0346e2
Cessalan <31516973+Cessalan@users.noreply.github.com>
2019-12-13 20:50:59 -0500

    DBManager added

---
+++ DB/DBManager.php
@@ -0,0 +1,53 @@
+<?php
+function connection(){
+
+    $conn = new mysqli("127.0.0.1","root","","gcc");
+    if($conn->error){
+        echo $conn->error;
+    }
+    return $conn;
+}
+
+//search for account int the database when user logs in
+function  getAccount($email,$password){
+    $conn=connection();
+    $sqlGetAccount="SELECT email,password FROM  account WHERE email='".$email."'and password='".$password."'";
+    $result=$conn->query($sqlGetAccount);
+
+    if($result->num_rows<1){
+        return false;
+    }else{
+        return true;
+    }
+
+}
+
+//get hours for a specific date
+function getHours($date){
+    $conn=connection();
+    $sqlGetHours="SELECT time FROM appointment WHERE date='".$date."'";
+    $result=$conn->query($sqlGetHours);
+
+    if($result->num_rows>1){
+
+    }else if($result->num_rows>3){
+
+    }else{
+
+
+    }
+}
+
+//create account in db when admin creates account
+function insertAccount($email,$username,$password){
+    $conn=connection();
+    $sqlCreateAccount="";
+}
+
+//insert appointment in the db when users books an appointment
+function insertAppointment(){
+  $conn=connection();
+  $sqlInsertAppointment="";
+}
+
+
--- controllers/Account_Login.php
+++ controllers/Account_Login.php
@@ -1,19 +1,8 @@
 <?php
-include('../DB/connection.php');
+include('../DB/DBManager.php');
+
 $mail=$_GET['email'];
 $pass=$_GET['pass'];
-function  getAccount($email,$password){
-$conn=connection();
-$sqlGetAccount="SELECT email,password FROM  account WHERE email='".$email."'and password='".$password."'";
-$result=$conn->query($sqlGetAccount);
-
-if($result->num_rows<1){
-return false;
-}else{
-return true;
-}
-
-}
 
 if(getAccount($mail,$pass)){
 
