0a5c52217fdb87565275e70d32ed649b49dde4fa
haroon masjedi <haroon_masjide@hotmail.com>
2020-01-16 14:42:54 -0500

    Commit

---
+++ inc/config.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+// Set Language variable
+if(isset($_GET['lang']) && !empty($_GET['lang'])){
+    $_SESSION['lang'] = $_GET['lang'];
+}
+
+// Include Language file
+if(isset($_SESSION['lang']) && $_SESSION['lang'] == "en"){
+    include_once("../assets/lang/lang_en.php");
+
+}
+else{
+    include_once("../assets/lang/lang_fr.php");
+}
+?>
+<script>
+    function changeLang(){
+        document.getElementById('form_lang').submit();
+    }
+</script>
+
+
