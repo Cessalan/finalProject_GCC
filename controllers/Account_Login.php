fe69e53b7e0984f9247c76409d58295e32cb50d1
Cessalan <31516973+Cessalan@users.noreply.github.com>
2020-01-29 14:36:20 -0500

    modified admin login

--- controllers/Account_Login.php
+++ controllers/Account_Login.php
@@ -6,8 +6,8 @@ $mail=$_GET['email'];
 $pass=$_GET['pass'];
 
 
-if(getAccount($mail,$pass)=="logged" && $mail=="garagecheminchambly@gmail.com "){
-    $createAccount="../views/CreateAccount.php";
+if(getAccount($mail,$pass)=="logged" && $mail=="garagecheminchambly@gmail.com"){
+    $createAccount="../views/AdminPanel.php";
     header("location:".$createAccount);
 
 }else if(getAccount($mail,$pass)=="logged"){
